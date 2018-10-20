bd9b7059ccca17253d999e01fb41de27355a4d5b
rodrigoejcm <rodrigo.ejcm@gmail.com>
2018-10-20 19:40:15 +0100

    incuindo o grafico

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import pandas as pd
 from flask import Flask, render_template
 from pony.orm import *
 import db_init
+import matplotlib.pyplot as plt
+import io
+import base64
 
 app = Flask(__name__)
 
@@ -16,12 +19,14 @@ def index():
     last_five_tweets = find_last_tweets()
     retweet_tweets = find_most_retweet_tweets()
     reply_tweets = find_most_reply_tweets()
+    graph = get_total_tweets_hour()
     print(reply_tweets)
     return render_template('index.html',
         last_tweets=last_five_tweets,
         reply_tweets=reply_tweets, 
         retweet_tweets=retweet_tweets,
-        total=total_tweets, 
+        total=total_tweets,
+        graph = graph, 
         space = total_space/10 **9,
         used = used_space/10**9,
         free = free_space/10**9 )
@@ -40,10 +45,30 @@ def find_most_retweet_tweets():
 def count_tweets():
     return select((count(t)) for t in db_init.Tweet)[:1]
 
+def get_total_tweets_hour():
+        t = db_init.db.execute('SELECT DATE_FORMAT(created_at, "%Y-%m-%d %H") as date , count(*) from tweet group by DATE_FORMAT(created_at, "%Y-%m-%d %H") order by 1;')
+        x = []
+        y = []
+        for p in t.fetchall():
+                x.append(p[0])
+                y.append(p[1])
+        img = io.BytesIO()
+        plt.plot(x, y)
+        plt.xticks(rotation=45)
+        plt.margins(0.2)
+        plt.subplots_adjust(bottom=0.25)
+        for a,b in zip(x, y): 
+                plt.text(a, b, str(b))
+        plt.savefig(img, format='png')
+        img.seek(0)
+        graph_url = base64.b64encode(img.getvalue()).decode()
+        plt.close()
+        return 'data:image/png;base64,{}'.format(graph_url)
+
 
 if __name__ == '__main__':
     app.run(port=443, host='0.0.0.0')
-    #app.run()
+   # app.run()
 
 
 
