c5dfafa49ab3e8085967b31a242bd60be5a90086
rodrigoejcm <rodrigo.ejcm@gmail.com>
2018-10-20 18:37:43 +0100

    Ajustando os counts de replies e retweets

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def index():
     last_five_tweets = find_last_tweets()
     retweet_tweets = find_most_retweet_tweets()
     reply_tweets = find_most_reply_tweets()
+    print(reply_tweets)
     return render_template('index.html',
         last_tweets=last_five_tweets,
         reply_tweets=reply_tweets, 
@@ -30,18 +31,20 @@ def find_last_tweets():
     return db_init.Tweet.select().order_by(desc(db_init.Tweet.created_at))[:5]
 
 def find_most_reply_tweets():
-    return db_init.Tweet.select().order_by((db_init.Tweet.reply_count))[:5]
-    
+    return select((t.in_reply_to_status_id, count()) for t in db_init.Tweet if t.in_reply_to_status_id != None).order_by(desc(2))[:5]
+
+   
 def find_most_retweet_tweets():
-    return db_init.Tweet.select().order_by(desc(db_init.Tweet.retweet_count))[:5]
-
-
+    return select((t.retweet_from_tweet_id, count()) for t in db_init.Tweet if t.retweet_from_tweet_id != None).order_by(desc(2))[:5]
 
 def count_tweets():
     return select((count(t)) for t in db_init.Tweet)[:1]
 
 
-
 if __name__ == '__main__':
     app.run(port=443, host='0.0.0.0')
     #app.run()
+
+
+
+
